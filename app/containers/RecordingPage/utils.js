fbf1a2440f1af6b922f80c40a818baeab9fd9341
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-04-19 17:27:31 +0800

    removed log statement

--- app/containers/RecordingPage/utils.js
+++ app/containers/RecordingPage/utils.js
@@ -2,7 +2,6 @@
 Utility function to flatten nested objects for table display.
 */
 export function transformRows(data){
-  console.log(data)
   if(data){
     return data.map((obj) =>
       { return {...obj, performers: obj.performers.map((m) => m.name).join()}})
