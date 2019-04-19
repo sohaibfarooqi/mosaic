e18a9e06c4f9ea1ea300f716dc8501d7272362f8
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2019-04-19 15:47:45 +0800

    main app entry point

---
+++ app/app.js
@@ -0,0 +1,53 @@
+/**
+ * app.js
+ *
+ * This is the entry file for the application.
+ */
+
+// Needed for redux-saga es6 generator support
+import '@babel/polyfill';
+
+// Import all the third party stuff
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router/immutable';
+import FontFaceObserver from 'fontfaceobserver';
+import history from 'utils/history';
+import 'sanitize.css/sanitize.css';
+
+// Import root app
+import App from 'containers/App';
+
+import configureStore from './configureStore';
+
+// Observe loading of Open Sans (to remove open sans, remove the <link> tag in
+// the index.html file and this observer)
+const openSansObserver = new FontFaceObserver('Open Sans', {});
+
+// When Open Sans is loaded, add a font-family using Open Sans to the body
+openSansObserver.load().then(() => {
+  document.body.classList.add('fontLoaded');
+});
+
+// Create redux store with history
+const initialState = {};
+const store = configureStore(initialState, history);
+const MOUNT_NODE = document.getElementById('app');
+
+const render = () =>  {
+  ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <App />
+        </ConnectedRouter>
+    </Provider>,
+    MOUNT_NODE,
+  );
+};
+
+render();
+
+if (process.env.NODE_ENV === 'production') {
+  require('offline-plugin/runtime').install(); // eslint-disable-line global-require
+}
